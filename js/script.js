75a6e497269919ccbd20901c2d5b27f1f7b5043f
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-02-02 00:27:54 -0300

    Gerando caracteres aleatorios

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("teste")
+const generatePasswordButton = document.querySelector("#generate-password")
+const generatePasswordElement = document.querySelector("#generated-password")
+
+const getLetterLowerCase = () => {
+    return string.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+const getLetterUpperCase = () => {
+    return string.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/.,!@#$%¨¨&*-+|?";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+generatePasswordButton.addEventListener("click", () => {
+    console.log("Teste")
+})
